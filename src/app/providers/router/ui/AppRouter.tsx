a5286cbef8f03823904e1ccc1bf5024b0a815d5e
Rustem <gladius.dancer@gmail.com>
2023-06-19 19:54:13 +0500

    Preloader, Router

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,14 +2,14 @@ import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routerConfig/routerConfig';
 import { useTranslation } from 'react-i18next';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const AppRouter = () => {
     const { t } = useTranslation();
     return (
         <Suspense fallback={(
             <div>
-                {t('Loading')}
-                ...
+                <PageLoader />
             </div>
         )}
         >
@@ -21,8 +21,7 @@ const AppRouter = () => {
                             <div className="page-wrapper">
                                 <Suspense fallback={(
                                     <div>
-                                        {t('Loading')}
-                                        ...
+                                        <PageLoader />
                                     </div>
                                 )}
                                 >
